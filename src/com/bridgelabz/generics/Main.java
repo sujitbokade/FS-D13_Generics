3fb6c909d5fbce4a4b6208a3564193b5361a8de8
sujit <sujitbokade2014@gmail.com>
2022-07-26 16:23:57 +0530

    [Sujit]Find Maximum String

--- src/com/bridgelabz/generics/Main.java
+++ src/com/bridgelabz/generics/Main.java
@@ -2,13 +2,13 @@ package com.bridgelabz.generics;
 
 public class Main {
     public static void main(String[] args) {
-        MaxFloat maxFloat=new MaxFloat();
+        MaxString maxString=new MaxString();
         System.out.println("First Test Case:-");
-        maxFloat.maxValue(7.2F,4.5F,9.4F);
+        maxString.maxValue("Apple","Peach","Banana");
         System.out.println("Second Test Case:-");
-        maxFloat.maxValue(4.4F,8.6F,2.5F);
+        maxString.maxValue("Banana","Mango","coconut");
         System.out.println("Third Test Case:-");
-        maxFloat.maxValue(6.4F,5.4F,2.3F);
+        maxString.maxValue("Apple","Orange","Carrot");
     }
 
 }
