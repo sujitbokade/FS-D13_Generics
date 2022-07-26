fce76721a95465ef3aa73119a1a31264aba8a5f9
sujit <sujitbokade2014@gmail.com>
2022-07-26 16:25:54 +0530

    [Sujit]Find Maximum Float

--- src/com/bridgelabz/generics/MaxFloat.java
+++ src/com/bridgelabz/generics/MaxFloat.java
@@ -3,11 +3,11 @@ package com.bridgelabz.generics;
 public class MaxFloat {
     void maxValue(Float val1,Float val2,Float val3){
         if((val1.compareTo(val2)>=0) && (val1.compareTo(val3)>=0)){
-            System.out.println(val1+" is the Largest Integer");
+            System.out.println(val1+" is the Largest Float");
         } else if ((val2.compareTo(val1)>=0) && (val2.compareTo(val3)>=0)) {
-            System.out.println(val2+" is the Largest Integer");
+            System.out.println(val2+" is the Largest Float");
         }else {
-            System.out.println(val3+" is the Largest Integer");
+            System.out.println(val3+" is the Largest Float");
         }
     }
 }
